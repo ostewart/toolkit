8534f9372b381897e666aac16e530975e884f9f3
Oliver Stewart <oliver@trailmagic.com>
2004-03-09 05:25:58 +0000

    Made most columns not-null. Added image_data column to the ImageManifestation mapping and class. Made image_id not null on image_manifestations. Added properties to hibernate.properties to support schema updates.

--- imagestore/src/com/trailmagic/image/ImageManifestation.java
+++ imagestore/src/com/trailmagic/image/ImageManifestation.java
@@ -1,5 +1,7 @@
 package com.trailmagic.image;
 
+import java.sql.Blob;
+
 public class ImageManifestation implements Comparable {
     private long m_id;
     private Image m_image;
@@ -8,6 +10,7 @@ public class ImageManifestation implements Comparable {
     private int m_width;
     private String m_format;
     private boolean m_original;
+    private Blob m_data;
     
     public ImageManifestation() {
     }
@@ -66,6 +69,14 @@ public class ImageManifestation implements Comparable {
 
     public void setOriginal(boolean original) {
         m_original = original;
+    }
+
+    public Blob getData() {
+        return m_data;
+    }
+
+    public void setData(Blob data) {
+        m_data = data;
     }
     
     private int getArea() {
