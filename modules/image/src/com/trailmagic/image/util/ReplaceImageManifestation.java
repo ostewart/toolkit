aa49293a24a05c694c780dfbaa1cdf8124d6847c
Oliver Stewart <oliver@trailmagic.com>
2006-05-21 02:28:46 +0000

    Added ReplaceImageManifestation code to replace an entire ImageGroup Fixed weird striping bug in image resizing by doing the rotate before the  resize operations Added some transaction interceptors

--- modules/image/src/com/trailmagic/image/util/ReplaceImageManifestation.java
+++ modules/image/src/com/trailmagic/image/util/ReplaceImageManifestation.java
@@ -14,18 +14,29 @@
 package com.trailmagic.image.util;
 
 import com.trailmagic.image.HeavyImageManifestation;
+import com.trailmagic.image.Image;
+import com.trailmagic.image.ImageFrame;
+import com.trailmagic.image.ImageGroup;
+import com.trailmagic.image.ImageGroupFactory;
+import com.trailmagic.image.ImageManifestation;
 import com.trailmagic.image.ImageManifestationFactory;
+import com.trailmagic.user.User;
+import com.trailmagic.user.UserFactory;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
+import javax.imageio.ImageIO;
+import org.apache.log4j.Logger;
 import org.hibernate.Hibernate;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.orm.hibernate3.HibernateCallback;
+import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.orm.hibernate3.SessionFactoryUtils;
 
 
@@ -38,6 +49,10 @@ import org.springframework.orm.hibernate3.SessionFactoryUtils;
 public class ReplaceImageManifestation {
     private SessionFactory m_sessionFactory;
     private ImageManifestationFactory m_imfFactory;
+    private HibernateTemplate m_hibernateTemplate;
+    private UserFactory m_userFactory;
+    private ImageGroupFactory m_imageGroupFactory;
+
     private static Logger s_log =
         Logger.getLogger(ReplaceImageManifestation.class);
     private static final String REPLACEIM_BEAN = "replaceImageManifestation";
@@ -56,14 +71,23 @@ public class ReplaceImageManifestation {
      **/
     public void setSessionFactory(SessionFactory sf) {
         m_sessionFactory = sf;
-        s_log.debug(this + "setSessionFactory called with " + sf + "("
-                    + m_sessionFactory + ")" + Thread.currentThread());
     }
 
     public void setImageManifestationFactory(ImageManifestationFactory factory) {
         m_imfFactory = factory;
         s_log.debug("setImageManifestationFactory called on "
                     + this + " with " + m_imfFactory);
+    }
+
+    public void setHibernateTemplate(HibernateTemplate template) {
+        m_hibernateTemplate = template;
+    }
+
+    public void setUserFactory(UserFactory factory) {
+        m_userFactory = factory;
+    }
+    public void setImageGroupFactory(ImageGroupFactory factory) {
+        m_imageGroupFactory = factory;
     }
 
     /**
@@ -79,7 +103,7 @@ public class ReplaceImageManifestation {
 
             HeavyImageManifestation manifest =
                 m_imfFactory.getHeavyById(manifestationId.longValue());
-            s_log.info("Got manifestation id " + manifest.getId());
+            //            s_log.info("Got manifestation id " + manifest.getId());
 
             File srcFile = new File(filename);
             s_log.info("Importing " + srcFile.getPath());
@@ -114,6 +138,76 @@ public class ReplaceImageManifestation {
         }
     }
 
+    public void replaceManifestations(final String ownerName,
+                                      final String rollName,
+                                      final String importDir) {
+        m_hibernateTemplate.execute(new HibernateCallback() {
+                public Object doInHibernate(Session session) {
+                    try {
+                        User owner = m_userFactory.getByScreenName(ownerName);
+                        ImageGroup roll =
+                            m_imageGroupFactory
+                            .getRollByOwnerAndName(owner, rollName);
+                        s_log.info("Processing roll: " + roll.getName());
+                        for (ImageFrame frame : roll.getFrames()) {
+                            Image image = frame.getImage();
+                            for (ImageManifestation mf
+                                     : image.getManifestations()) {
+
+                                String filename = importDir + File.separator
+                                    + mf.getName();
+                                File srcFile = new File(filename);
+                                s_log.info("Importing " + srcFile.getPath());
+                                if ( srcFile.length() > Integer.MAX_VALUE ) {
+                                    s_log.info("File is too big...skipping "
+                                               + srcFile.getPath());
+                                    throw new RuntimeException("File too big");
+                                }
+
+                                // first read the file to get the size
+                                FileInputStream fis =
+                                    new FileInputStream(srcFile);
+                                BufferedImage bi =
+                                    ImageIO.read(fis);
+                                mf.setHeight(bi.getHeight());
+                                mf.setWidth(bi.getWidth());
+                                s_log.info("New size is " + mf.getHeight()
+                                           + "x" + mf.getWidth());
+                                session.saveOrUpdate(mf);
+                                fis.close();
+
+                                fis = new FileInputStream(srcFile);
+                                HeavyImageManifestation heavyMf =
+                                    m_imfFactory.getHeavyById(mf.getId());
+                                heavyMf.setData(Hibernate.createBlob(fis));
+                                session.saveOrUpdate(heavyMf);
+
+                                // open the file again to get the size :(
+
+                                s_log.info("ImageManifestation saved: "
+                                           + heavyMf.getName()
+                                           + " (" + heavyMf.getId() + ")"
+                                           + "...flushing session and evicting "
+                                           + "manifestation.");
+
+                                synchronized (session) {
+                                    session.flush();
+                                    session.evict(heavyMf);
+                                }
+
+                                // looks like fis has to be open for the flush
+                                fis.close();
+
+                            }
+                        }
+                    } catch (Exception e) {
+                        s_log.error("Error replacing manifestations ", e);
+                    }
+                    return null;
+                }
+            });
+    }
+
     private static void printUsage() {
         System.out.println("Usage: ReplaceImageManifestation "
                            + "<manifestation-id> <filename> <width> <height>");
@@ -121,7 +215,7 @@ public class ReplaceImageManifestation {
 
 
     public static final void main(String[] args) {
-        if ( args.length != 4 ) {
+        if ( args.length != 4 && args.length != 3) {
             printUsage();
             System.exit(1);
         }
@@ -129,7 +223,11 @@ public class ReplaceImageManifestation {
 
         ClassPathXmlApplicationContext appContext =
             new ClassPathXmlApplicationContext(new String[]
-                {"applicationContext-standalone.xml"});
+                {"applicationContext-global.xml",
+                 "applicationContext-user.xml",
+                 "applicationContext-imagestore.xml",
+                 "applicationContext-imagestore-authorization.xml",
+                 "applicationContext-standalone.xml"});
 
         ReplaceImageManifestation worker =
             (ReplaceImageManifestation) appContext.getBean(REPLACEIM_BEAN);
@@ -137,6 +235,11 @@ public class ReplaceImageManifestation {
         s_log.debug("got bean " + worker
                     + "with sf: " + worker.getSessionFactory()
                     + " and IMFactory: " + worker.m_imfFactory);
+
+        if (args.length == 3) {
+            worker.replaceManifestations(args[0], args[1], args[2]);
+            System.exit(0);
+        }
 
         try {
             Long manifestId = new Long(args[0]);
