a06abd9dbcbbaac70cd9fb977256271be62e6e37
Oliver Stewart <oliver@trailmagic.com>
2008-05-03 18:41:20 +0000

    Moved to maven: Changed directory structure to fit maven standard Moved to JEE5 (servlet 2.5 mostly) Removed resin dependency (now runs in tomcat!) Setup as dynamic web module in eclipse Misc improvements/updates to make everything run in maven/wtp Now all *.html and *.css will be served with default servlet

---
+++ photo/src/main/java/com/trailmagic/image/ui/ImageGroupLinkTag.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2006 Oliver Stewart.  All Rights Reserved.
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package com.trailmagic.image.ui;
+
+import javax.servlet.jsp.tagext.TagSupport;
+import javax.servlet.jsp.JspException;
+import java.io.IOException;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+import javax.servlet.http.HttpServletRequest;
+
+import com.trailmagic.user.*;
+import com.trailmagic.image.*;
+
+public class ImageGroupLinkTag extends TagSupport {
+    private ImageFrame m_frame = null;
+    private User m_owner = null;
+    private ImageGroup m_imageGroup = null;
+    private Long m_image = null;
+    private ImageGroup.Type m_groupType = null;
+    private String m_styleClass = null;
+
+    public void setOwner(User owner) {
+        m_owner = owner;
+    }
+
+    public User getOwner() {
+        return m_owner;
+    }
+
+    public void setImageGroup(ImageGroup imageGroup) {
+        m_imageGroup = imageGroup;
+    }
+
+    public ImageGroup getImageGroup() {
+        return m_imageGroup;
+    }
+
+    public void setGroupType(String groupType) {
+        this.m_groupType = ImageGroup.Type.fromString(groupType);
+    }
+
+    public String getGroupType() {
+        return m_groupType.toString();
+    }
+
+    public void setImage(Long image) {
+        m_image = image;
+    }
+
+    public Long getImage() {
+        return m_image;
+    }
+
+    public void setStyleClass(String styleClass) {
+        m_styleClass = styleClass;
+    }
+
+    public String getStyleClass() {
+        return m_styleClass;
+    }
+
+    // getClass is already taken
+
+    public int doStartTag() throws JspException {
+        StringBuffer html = new StringBuffer();
+
+        try {
+            html.append("<a ");
+            if (m_styleClass != null) {
+                html.append("class=\"" + m_styleClass + "\" ");
+            }
+            html.append("href=\"");
+
+            //XXX: yeek?
+            WebApplicationContext ctx =
+                WebApplicationContextUtils
+                .getRequiredWebApplicationContext(pageContext
+                                                  .getServletContext());
+            LinkHelper helper =
+                (LinkHelper)ctx.getBean("linkHelper");
+            // XXX: check for null bean
+            // XXX: evil cast?
+            helper.setRequest((HttpServletRequest)pageContext.getRequest());
+
+            if ( m_image != null ) {
+                html.append(helper.getImageGroupFrameUrl(m_imageGroup, m_image));
+            } else if ( m_imageGroup != null ) {
+                html.append(helper.getImageGroupUrl(m_imageGroup));
+            } else if ( m_owner != null ) {
+                html.append(helper.getImageGroupsUrl(m_groupType, m_owner));
+            } else {
+                html.append(helper.getImageGroupsRootUrl(m_groupType));
+            }
+            html.append("\">");
+            pageContext.getOut().write(html.toString());
+            return EVAL_BODY_INCLUDE;
+        } catch (IOException e) {
+            throw new JspException(e);
+        }
+    }
+
+    public int doEndTag() throws JspException {
+        try {
+            pageContext.getOut().write("</a>");
+            return EVAL_PAGE;
+        } catch (IOException e) {
+            throw new JspException(e);
+        }
+    }
+
+    public void setFrame(ImageFrame frame) {
+        m_frame = frame;
+    }
+
+    public ImageFrame getFrame() {
+        return m_frame;
+    }
+}
---
+++ photo/src/main/webapp/WEB-INF/jsp/imageGroupAdd.jsp
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<!--
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="tm" uri="http://trailmagic.com/taglibs/image" %>
+
+View for ImageGroup display.
+
+Model Requirements:
+user: currently logged in user
+owner: the current album's owner
+
+-->
+<html>
+  <head>
+    <title>New Image Group</title>
+  </head>
+
+  <body>
+    <h1>New Image Group</h1>
+    <spring:hasBindErrors name="imageImportBean">
+      <c:forEach var="error" items="${errors.allErrors}">
+      <c:out value="${error}"/>
+      </c:forEach>
+    </spring:hasBindErrors>
+
+    <spring:hasBindErrors name="com.trailmagic.image.image.ImageGroup">
+      (full class name)
+      <c:forEach var="error" items="${errors.allErrors}">
+        <c:out value="${error}"/>
+      </c:forEach>
+    </spring:hasBindErrors>
+
+    <form method="POST" action="." enctype="multipart/form-data">
+      <table>
+          <tr>
+            <td><label for="name">Name:</label></td>
+            <td><input type="text" name="name"/></td>
+          </tr>
+          <tr>
+            <td><label for="displayName">Display Name:</label></td>
+            <td><input type="text" name="displayName"/></td>
+          </tr>
+          <tr>
+            <td><label for="description">Description:</label></td>
+            <td><input type="text" name="description"/></td>
+          </tr>
+      </table>
+    </form>
+  </body>
+</html>
