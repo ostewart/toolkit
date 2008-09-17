0856f083042887004f2775943dd3225441bdeca4
Oliver Stewart <oliver@trailmagic.com>
2008-09-17 05:47:32 +0000

    Factored image group display out of ImageGroupController, renamed to ImageGroupDispatcherController

--- photo/src/main/java/com/trailmagic/image/ui/ImageGroupDispatcherController.java
+++ photo/src/main/java/com/trailmagic/image/ui/ImageGroupDispatcherController.java
@@ -14,11 +14,8 @@
 package com.trailmagic.image.ui;
 
 import com.trailmagic.image.Image;
-import com.trailmagic.image.ImageFrame;
 import com.trailmagic.image.ImageGroup;
 import com.trailmagic.image.ImageGroupRepository;
-import com.trailmagic.image.NoSuchImageGroupException;
-import com.trailmagic.image.security.ImageSecurityFactory;
 import com.trailmagic.user.User;
 import com.trailmagic.user.UserFactory;
 import com.trailmagic.web.util.ImageRequestInfo;
@@ -28,16 +25,13 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.SortedSet;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.acegisecurity.AccessDeniedException;
 import org.apache.log4j.Logger;
-import org.springframework.web.bind.ServletRequestUtils;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
@@ -45,28 +39,26 @@ import org.springframework.web.servlet.mvc.Controller;
  * to add a new group type, simply add a new mapping to this controller as
  * "/<type-name>s/**" in the handlerMapping in images-servlet.xml
  **/
-public class ImageGroupController implements Controller {
+public class ImageGroupDispatcherController implements Controller {
     private static final String LIST_VIEW = "imageGroupList";
     private static final String USERS_VIEW = "imageGroupUsers";
-    private static final String IMG_GROUP_VIEW = "imageGroup";
-    private static final String IMAGE_DISPLAY_VIEW = "imageDisplay";
 
     private static Logger log =
-        Logger.getLogger(ImageGroupController.class);
+        Logger.getLogger(ImageGroupDispatcherController.class);
 
-    private ImageSecurityFactory imageSecurityFactory;
     private ImageGroupRepository imageGroupRepository;
     private ImageDisplayController imageDisplayController;
+    private ImageGroupDisplayController imageGroupDisplayController;
     private UserFactory userFactory;
     private WebRequestTools webRequestTools;
 
-    public ImageGroupController(ImageSecurityFactory imageSecurityFactory,
-                                ImageGroupRepository imageGroupRepository,
-                                ImageDisplayController imageDisplayController,
-                                UserFactory userFactory,
-                                WebRequestTools webRequestTools) {
+    public ImageGroupDispatcherController(ImageGroupDisplayController imageGroupDisplayController,
+                                          ImageGroupRepository imageGroupRepository,
+                                          ImageDisplayController imageDisplayController,
+                                          UserFactory userFactory,
+                                          WebRequestTools webRequestTools) {
         super();
-        this.imageSecurityFactory = imageSecurityFactory;
+        this.imageGroupDisplayController = imageGroupDisplayController;
         this.imageGroupRepository = imageGroupRepository;
         this.userFactory = userFactory;
         this.webRequestTools = webRequestTools;
@@ -122,8 +114,9 @@ public class ImageGroupController implements Controller {
 
         // got user and group args: show one group
         if (iri.getImageId() == null) {
-            return handleDisplayGroup(model, iri.getImageGroupType(),
-                                      iri.getScreenName(), iri.getImageGroupName());
+//            return handleDisplayGroup(model, iri.getImageGroupType(),
+//                                      iri.getScreenName(), iri.getImageGroupName());
+            return imageGroupDisplayController.handleDisplayGroup(req, new ModelMap(model));
         }
         
 //        return handleDisplayFrame(iri.getImageGroupName(), iri.getImageId(),
@@ -131,91 +124,6 @@ public class ImageGroupController implements Controller {
         return imageDisplayController.handleRequest(req, res);
     }
     
-    private boolean isEditMode(HttpServletRequest request) throws ServletException {
-        String mode = ServletRequestUtils.getStringParameter(request, "mode");
-        if ("edit".equals(mode)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    private ModelAndView handleDisplayGroup(Map<String, Object> model,
-                                            ImageGroup.Type groupType,
-                                            String ownerName,
-                                            String groupName)
-            throws NoSuchImageGroupException {
-        User owner = userFactory.getByScreenName(ownerName);
-        ImageGroup group =
-            imageGroupRepository.getByOwnerNameAndTypeWithFrames(owner,
-                                                                 groupName,
-                                                                 groupType);
-        model.put("imageGroup", group);
-        model.put("imageGroupIsPublic",
-                  imageSecurityFactory.isPublic(group));
-
-        SortedSet<ImageFrame> frames = group.getFrames();
-        log.debug("Frames contains " + frames.size() + " items.");
-        model.put("frames", frames);
-
-        return new ModelAndView(IMG_GROUP_VIEW, model);
-    }
-
-    private ModelAndView handleDisplayFrame(String groupName,
-                                            long frameId,
-                                            Map<String, Object> model,
-                                            boolean isEditView,
-                                            String requestUri) {
-        ImageFrame frame =
-            imageGroupRepository.getImageFrameByGroupNameAndImageId(groupName,
-                                                                    frameId);
-        model.put("requestUri", requestUri);
-        model.put("isEditView", isEditView);
-        model.put("frame", frame);
-        model.put("image", frame.getImage());
-        model.put("group", frame.getImageGroup());
-        model.put("imageIsPublic",
-                  imageSecurityFactory.isPublic(frame.getImage()));
-        List<ImageGroup> groupsContainingImage =
-            imageGroupRepository.getByImage(frame.getImage());
-        List<ImageGroup> otherGroups = new ArrayList<ImageGroup>();
-        Iterator<ImageGroup> iter = groupsContainingImage.iterator();
-        ImageGroup containingGroup;
-        while (iter.hasNext()) {
-            containingGroup = iter.next();
-            if ( !frame.getImageGroup().equals(containingGroup) ) {
-                otherGroups.add(containingGroup);
-            }
-        }
-        model.put("groupsContainingImage", otherGroups);
-
-        SortedSet<ImageFrame> frames = frame.getImageGroup().getFrames();
-        SortedSet<ImageFrame> tmpSet = frames.headSet(frame);
-
-//             Iterator iter = tmpSet.iterator();
-//             iter.next();
-//             if ( iter.hasNext() ) {
-//                 ImageFrame prevFrame = (ImageFrame)iter.next();
-//             }
-
-
-        if ( !tmpSet.isEmpty() ) {
-            ImageFrame prevFrame = tmpSet.last();
-            model.put("prevFrame", prevFrame);
-        }
-
-        tmpSet = frames.tailSet(frame);
-        Iterator<ImageFrame> framesIter = tmpSet.iterator();
-        framesIter.next();
-        if ( framesIter.hasNext() ) {
-            ImageFrame nextFrame = framesIter.next();
-            model.put("nextFrame", nextFrame);
-        }
-
-        // got user, group, and frame number: show that frame
-        return new ModelAndView(IMAGE_DISPLAY_VIEW, model);
-    }
-
     private ModelAndView handleListUsers(ImageGroup.Type groupType,
                                          Map<String, Object> model) {
         model.put("owners", imageGroupRepository.getOwnersByType(groupType));
---
+++ photo/src/main/java/com/trailmagic/image/ui/ImageGroupDisplayController.java
@@ -0,0 +1,72 @@
+package com.trailmagic.image.ui;
+
+import com.trailmagic.image.ImageFrame;
+import com.trailmagic.image.ImageGroup;
+import com.trailmagic.image.ImageGroupRepository;
+import com.trailmagic.image.NoSuchImageGroupException;
+import com.trailmagic.image.security.ImageSecurityFactory;
+import com.trailmagic.user.User;
+import com.trailmagic.user.UserFactory;
+import com.trailmagic.web.util.ImageRequestInfo;
+import com.trailmagic.web.util.MalformedUrlException;
+import com.trailmagic.web.util.WebRequestTools;
+import java.util.SortedSet;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ImageGroupDisplayController {
+    private UserFactory userFactory;
+    private ImageGroupRepository imageGroupRepository;
+    private ImageSecurityFactory imageSecurityFactory;
+    private WebRequestTools webRequestTools;
+    
+    private static final String IMG_GROUP_VIEW = "imageGroup";
+    private static Logger log =
+        Logger.getLogger(ImageGroupDisplayController.class);
+    
+    
+    public ImageGroupDisplayController(ImageGroupRepository imageGroupRepository,
+                                       ImageSecurityFactory imageSecurityFactory,
+                                       UserFactory userFactory,
+                                       WebRequestTools webRequestTools) {
+        super();
+        this.imageGroupRepository = imageGroupRepository;
+        this.imageSecurityFactory = imageSecurityFactory;
+        this.userFactory = userFactory;
+        this.webRequestTools = webRequestTools;
+    }
+
+    @RequestMapping(method=RequestMethod.GET)
+    public ModelAndView handleDisplayGroup(HttpServletRequest request,
+                                           ModelMap model)
+            throws NoSuchImageGroupException, MalformedUrlException {
+        
+        ImageRequestInfo iri = webRequestTools.getImageRequestInfo(request);
+        User owner = userFactory.getByScreenName(iri.getScreenName());
+        ImageGroup group =
+            imageGroupRepository.getByOwnerNameAndTypeWithFrames(owner,
+                                                                 iri.getImageGroupName(),
+                                                                 iri.getImageGroupType());
+        model.addAttribute("imageGroup", group);
+        model.addAttribute("imageGroupIsPublic",
+                           imageSecurityFactory.isPublic(group));
+
+        SortedSet<ImageFrame> frames = group.getFrames();
+        log.debug("Frames contains " + frames.size() + " items.");
+        model.addAttribute("frames", frames);
+
+        return new ModelAndView(IMG_GROUP_VIEW, model);
+    }
+    
+    @RequestMapping(method=RequestMethod.POST)
+    public void processSubmit(@ModelAttribute("imageGroup") ImageGroup imageGroup) {
+        
+    }
+}
