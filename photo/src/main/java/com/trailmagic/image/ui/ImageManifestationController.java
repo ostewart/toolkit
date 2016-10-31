9b67e52a0b6fa9b1495a7f6a60e9c5bbe458ab57
Oliver Stewart <oliver@trailmagic.com>
2016-10-31 12:53:59 -0400

    Stream out manifestation (hopefully) within a transaction

--- photo/src/main/java/com/trailmagic/image/ui/ImageManifestationController.java
+++ photo/src/main/java/com/trailmagic/image/ui/ImageManifestationController.java
@@ -15,15 +15,18 @@ package com.trailmagic.image.ui;
 
 import com.trailmagic.image.HeavyImageManifestation;
 import com.trailmagic.image.ImageManifestationRepository;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -39,6 +42,7 @@ public class ImageManifestationController {
     }
 
     @RequestMapping("/mf/by-id/{imageId}")
+    @Transactional(readOnly = true)
     public ModelAndView imageById(HttpServletResponse res, @PathVariable("imageId") Long imageId) throws Exception {
         Map<String, Object> model = new HashMap<String, Object>();
 
@@ -46,13 +50,14 @@ public class ImageManifestationController {
 
         java.io.InputStream dataStream = mf.getData().getBinaryStream();
         res.setContentLength((int) mf.getData().length());
-
-        log.debug("Passing manifestation data stream to view (type: " + dataStream.getClass() + ")");
-        model.put(InputStreamView.STREAM_KEY, dataStream);
-        model.put(InputStreamView.CONTENT_TYPE_KEY, mf.getFormat());
+        res.setContentType(mf.getFormat());
         if (mf.getName() != null) {
-            model.put(InputStreamView.CONTENT_DISPOSITION_KEY, "inline; filename=" + mf.getName() + ";");
+            res.setHeader("Content-Disposition", "inline; filename=" + mf.getName() + ";");
         }
-        return new ModelAndView(new InputStreamView(), model);
+
+        OutputStream out = res.getOutputStream();
+        IOUtils.copy(dataStream, out);
+
+        return null;
     }
 }
